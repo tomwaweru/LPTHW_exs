528f24eb555ab20b0e480fb285cc27741119271d
Tom Waweru <tomwaweru@gmail.com>
2022-10-28 13:47:04 +0300

    This is LPTHW commit

---
+++ ex5.py
@@ -0,0 +1,20 @@
+my_name = 'Tom Waweru'
+my_age = 36 # not a lie
+my_height = 6 # feet
+my_weight = 88 # Kg
+my_eyes = 'Brown'
+my_teeth = 'white'
+my_hair = 'Black'
+
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} feet tall")
+print(f"He's {my_weight} Kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+#this line is tricky, try to get exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
